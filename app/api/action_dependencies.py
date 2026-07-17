30a8d152269e3ac9f736f8d82eecb1449c1e13fe
shubham123dev <shubham.shrivastav@databridgemarketresearch.in>
2026-07-17 17:49:49 +0530

    wire hardened action lifecycle services

--- app/api/action_dependencies.py
+++ app/api/action_dependencies.py
@@ -27,17 +27,17 @@ from app.api.dependencies import (
 from app.permissions.permission_service import PermissionService
 from app.repositories.agent_action_repository import AgentActionRepository
 from app.repositories.audit_repository import AuditRepository
-from app.repositories.multi_approval_agent_action_repository import (
-    MultiApprovalAgentActionRepository,
+from app.repositories.hardened_agent_action_repository import (
+    HardenedAgentActionRepository,
 )
 from app.repositories.user_repository import UserRepository
 from app.services.agent_action_reconciliation_service import AgentActionReconciliationService
+from app.services.hardened_agent_action_service import HardenedAgentActionService
 from app.services.operational_resource_service import OperationalResourceService
-from app.services.stale_safe_agent_action_service import StaleSafeAgentActionService
 
 
 def get_agent_action_repository(session: SessionDep) -> AgentActionRepository:
-    return MultiApprovalAgentActionRepository(session)
+    return HardenedAgentActionRepository(session)
 
 
 def get_agent_action_registry() -> AgentActionRegistry:
@@ -70,8 +70,8 @@ def get_agent_action_service(
     action_repository: Annotated[AgentActionRepository, Depends(get_agent_action_repository)],
     action_registry: Annotated[AgentActionRegistry, Depends(get_agent_action_registry)],
     action_handlers: Annotated[dict[str, AgentActionHandler], Depends(get_agent_action_handlers)],
-) -> StaleSafeAgentActionService:
-    return StaleSafeAgentActionService(
+) -> HardenedAgentActionService:
+    return HardenedAgentActionService(
         organization_gateway=MockOrganizationApiAdapter(api),
         permission_service=PermissionService(user_repository),
         action_repository=action_repository,
@@ -83,7 +83,7 @@ def get_agent_action_service(
 
 def get_agent_action_reconciliation_service(
     action_service: Annotated[
-        StaleSafeAgentActionService,
+        HardenedAgentActionService,
         Depends(get_agent_action_service),
     ],
 ) -> AgentActionReconciliationService:
@@ -91,7 +91,7 @@ def get_agent_action_reconciliation_service(
 
 
 AgentActionServiceDep = Annotated[
-    StaleSafeAgentActionService,
+    HardenedAgentActionService,
     Depends(get_agent_action_service),
 ]
 AgentActionReconciliationServiceDep = Annotated[
